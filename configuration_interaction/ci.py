616b07ca25a0c70def630d2120426249d98a0151
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2019-02-28 13:54:29 +0100

    Take in numpy and store local variables

--- configuration_interaction/ci.py
+++ configuration_interaction/ci.py
@@ -2,9 +2,21 @@ import abc
 
 
 class ConfigurationInteraction(metaclass=abc.ABCMeta):
-    def __init__(self, system, verbose=False):
-        self.system = system
+    def __init__(self, system, verbose=False, np=None):
         self.verbose = verbose
+
+        if np is None:
+            import numpy as np
+
+        self.np = np
+
+        self.system = system
+
+        self.n = self.system.n
+        self.l = self.system.l
+        self.m = self.system.m
+        self.o = self.system.o
+        self.v = self.system.v
 
     @abc.abstractmethod
     def setup_ci_space(self):
