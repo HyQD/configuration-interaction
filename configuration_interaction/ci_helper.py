c4c39c37f256640321b9ec71541964b72e51c964
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2019-02-28 14:54:08 +0100

    Add relaxation of occupied particles

--- configuration_interaction/ci_helper.py
+++ configuration_interaction/ci_helper.py
@@ -2,25 +2,31 @@ import numba
 
 
 @numba.njit(cache=True)
-def create_reference_state(n, l, states, ref_index=0):
+def create_reference_state(n, l, states):
+    ref_index = 0
+
     for i in range(n):
         elem = i // 32
         states[ref_index, elem] |= 1 << (i - elem * 32)
 
-    for i in range(1, len(states)):
+    for i in range(ref_index + 1, len(states)):
         states[i] += states[ref_index]
 
 
-# @numba.njit(cache=True)
+@numba.njit(cache=True)
 def create_doubles_states(n, l, states, index):
-    for a in range(n, l):
-        elem_a = a // 32
-        for b in range(a + 1, l):
-            elem_b = b // 32
+    for i in range(n):
+        elem_i = i // 32
+        for j in range(i + 1, n):
+            elem_j = j // 32
+            for a in range(n, l):
+                elem_a = a // 32
+                for b in range(a + 1, l):
+                    elem_b = b // 32
 
-            states[index, elem_a] |= 1 << (a - elem_a * 32)
-            states[index, elem_b] |= 1 << (b - elem_b * 32)
+                    states[index, elem_i] ^= 1 << (i - elem_i * 32)
+                    states[index, elem_j] ^= 1 << (j - elem_j * 32)
+                    states[index, elem_a] |= 1 << (a - elem_a * 32)
+                    states[index, elem_b] |= 1 << (b - elem_b * 32)
 
-            index += 1
-
-    print(f"Index = {index}")
+                    index += 1
