7769faeb2220371c03ef5b0bf02b21c2364a2c31
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2019-02-28 14:54:37 +0100

    Print all full bit string

--- tests/test_cid.py
+++ tests/test_cid.py
@@ -10,11 +10,10 @@ def test_setup(odho_ti_small):
     for i in range(len(cid.states)):
         if cid.states[i, 0] > 0:
             counter += 1
-        print(
-            bin(cid.states[i, 1])[2:].zfill(odho_ti_small.l - 32)
-            + " "
-            + bin(cid.states[i, 0])[2:].zfill(32)
-        )
+
+        for elem in reversed(range(len(cid.states[i]))):
+            print(bin(cid.states[i, elem])[2:].zfill(32), end=" ")
+        print()
 
     print(counter, len(cid.states))
     assert False
