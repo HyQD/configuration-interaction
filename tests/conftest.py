0a4e5c98fe69e5082bc7f315dff5195405056057
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2019-02-28 14:54:56 +0100

    Test for smaller system

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,8 +5,8 @@ from quantum_systems import OneDimensionalHarmonicOscillator
 
 @pytest.fixture
 def odho_ti_small():
-    n = 20
-    l = 40
+    n = 2
+    l = 12
     grid = 10
     num_grid_points = 400
 
