5ee07dcb2aaf023a229f9f48b09f22258d021f3d
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2019-04-08 17:57:13 +0200

    Add skipped test of density for CIS and CISD

--- tests/test_cis.py
+++ tests/test_cis.py
@@ -1,7 +1,8 @@
+import pytest
 import numpy as np
 
 from configuration_interaction import CIS
-from configuration_interaction.ci_helper import BITSTRING_SIZE
+from configuration_interaction.ci_helper import BITSTRING_SIZE, state_printer
 
 
 def test_setup(odho_ti_small):
@@ -32,3 +33,28 @@ def test_slater_condon_hamiltonian(odho_ti_small):
 
     np.testing.assert_allclose(cis_b.hamiltonian, cis.hamiltonian, atol=1e-7)
     np.testing.assert_allclose(cis_b.energies, cis.energies)
+
+
+@pytest.mark.skip
+def test_slater_condon_density_matrix(odho_ti_small):
+    cis_b = CIS(odho_ti_small, brute_force=True, verbose=True)
+    cis_b.setup_ci_space()
+
+    cis = CIS(odho_ti_small, verbose=True)
+    cis.setup_ci_space()
+
+    np.testing.assert_allclose(cis_b.states, cis.states)
+
+    cis_b.compute_ground_state()
+    cis.compute_ground_state()
+
+    for K in range(len(cis.energies)):
+        print(f"K = {K}")
+        rho_b = cis_b.compute_one_body_density_matrix(K=K)
+        rho = cis.compute_one_body_density_matrix(K=K)
+
+        for i in np.ndindex(rho.shape):
+            if not abs(rho_b[i] - rho[i]) < 1e-8:
+                print(f"rho_b[{i}] = {rho_b[i]}\t|\trho[{i}] = {rho[i]}")
+
+        np.testing.assert_allclose(rho_b, rho, atol=1e-7)
--- tests/test_cisd.py
+++ tests/test_cisd.py
@@ -1,3 +1,4 @@
+import pytest
 import numpy as np
 
 from configuration_interaction import CISD
@@ -31,3 +32,30 @@ def test_slater_condon_hamiltonian(odho_ti_small):
 
     np.testing.assert_allclose(cisd_b.hamiltonian, cisd.hamiltonian, atol=1e-7)
     np.testing.assert_allclose(cisd_b.energies, cisd.energies)
+
+
+@pytest.mark.skip
+def test_slater_condon_density_matrix(odho_ti_small):
+    cisd_b = CISD(odho_ti_small, brute_force=True, verbose=True)
+    cisd_b.setup_ci_space()
+
+    cisd = CISD(odho_ti_small, verbose=True)
+    cisd.setup_ci_space()
+
+    np.testing.assert_allclose(cisd_b.states, cisd.states)
+
+    cisd_b.compute_ground_state()
+    cisd.compute_ground_state()
+
+    # Only check ground state as higher order states can be degenerate.
+    # This leads to ambiguity as to which state to compare.
+    K = 0
+
+    rho_b = cisd_b.compute_one_body_density_matrix(K=K)
+    rho = cisd.compute_one_body_density_matrix(K=K)
+
+    for i in np.ndindex(rho.shape):
+        if not abs(rho_b[i] - rho[i]) < 1e-8:
+            print(f"rho_b[{i}] = {rho_b[i]}\t|\trho[{i}] = {rho[i]}")
+
+    np.testing.assert_allclose(rho_b, rho, atol=1e-7)
