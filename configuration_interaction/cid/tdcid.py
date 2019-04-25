24b2d6767d4ba785bf92c8fc122c2a950c165a4e
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2019-04-25 13:17:40 +0200

    Delete unused repos and classes

--- configuration_interaction/cid/tdcid.py
+++
@@ -1,7 +0,0 @@
-from configuration_interaction import CID
-from configuration_interaction.tdci import TimeDependentConfigurationInteraction
-
-
-class TDCID(TimeDependentConfigurationInteraction):
-    def __init__(self, *args, **kwargs):
-        super().__init__(CID, *args, **kwargs)
--- configuration_interaction/cis/tdcis.py
+++
@@ -1,7 +0,0 @@
-from configuration_interaction import CIS
-from configuration_interaction.tdci import TimeDependentConfigurationInteraction
-
-
-class TDCIS(TimeDependentConfigurationInteraction):
-    def __init__(self, *args, **kwargs):
-        super().__init__(CIS, *args, **kwargs)
